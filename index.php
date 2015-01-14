67aeb9eaa6793c7ecf873ed25928a86fd9306f92
Mark Heideman <cptdpft@erthlink.net>
2015-01-14 09:34:36 -0700

    added gitnore and placeholder.

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<title>Personal Web Site</title>
+	</head>
+	<body>
+		<p>This is my personal web site. It is going to be awsome, confusing, and RAD in 8 weeks. (I hope).</p>
+	</body>
+</html>
